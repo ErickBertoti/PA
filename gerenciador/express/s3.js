6d43939074e4caa9b13dcd0c16f3604e5dc1282b
ErickBertoti <erickhoffmann2301@gmail.com>
2024-11-18 01:09:04 -0300

    Amanhã continuamos

--- gerenciador/express/s3.js
+++ gerenciador/express/s3.js
@@ -47,7 +47,7 @@ export async function getObjectSignedUrl(key) {
 
   // https://aws.amazon.com/blogs/developer/generate-presigned-url-modular-aws-sdk-javascript/
   const command = new GetObjectCommand(params);
-  const seconds = 60
+  const seconds = 60 * 5
   const url = await getSignedUrl(s3Client, command, { expiresIn: seconds });
 
   return url
