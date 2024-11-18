7927dfbe14e918312eee547fbb6759299a2d435e
ErickBertoti <erickhoffmann2301@gmail.com>
2024-11-17 22:10:32 -0300

    Tá funcionando

--- gerenciador/backend/src/routes/fileRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { listFilesFromS3 } from '../controllers/fileController';  // Importando o controlador para listar arquivos
-
-const router = express.Router();
-
-// Rota para listar arquivos do S3
-router.get('/', listFilesFromS3);  // Vai acessar '/files' no backend
-
-export default router;
